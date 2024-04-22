4a078b5584b5914ca4c35a47d34443d19faa25a8
ahosseini06 <ali20060101@gmail.com>
2024-04-21 21:41:12 -0700

    mobile ui bug fix

--- website-dev/src/components/candidate/CandidateModal.jsx
+++ website-dev/src/components/candidate/CandidateModal.jsx
@@ -63,7 +63,7 @@ const CandidateModal = ({ open, onClose, candidate }) => {
             )}
           </div>
           <div id="right container" className={styles.modalRightContainer}>
-            <h3 style={{ marginTop: "-3rem", paddingTop: "0" }}>
+            <h3 style={{ marginTop: "0", paddingTop: "0" }}>
               {candidate &&
                 candidate.vision &&
                 makePosessive(candidate.firstName) +
