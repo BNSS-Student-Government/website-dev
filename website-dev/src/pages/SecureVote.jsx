a2b1c21b69f6e30968c91e22cfdaec63360fb744
ahosseini06 <ali20060101@gmail.com>
2024-04-25 10:36:50 -0700

    form embed system.

--- website-dev/src/pages/SecureVote.jsx
+++ website-dev/src/pages/SecureVote.jsx
@@ -4,39 +4,40 @@ import styles from "./styles/SecureVote.module.css";
 
 const SecureVote = () => {
   const [stage, setStage] = useState(0);
+  const [grade, setGrade] = useState(0);
   const currentDate = new Date();
   /* voting open April 26th of April at 7:00AM */
   const votingOpenDate = new Date("2024-04-26T07:30:00");
   /* voting closes April 26th of April at 11:59AM */
   const votingCloseDate = new Date("2024-04-26T10:37:00");
 
-  const gr8link = "https://forms.office.com/r/BCXwmvcDWC";
-  const gr9link = "https://forms.office.com/r/RapBFPticA";
-  const gr10link = "https://forms.office.com/r/Xs7MeBukXM";
-  const gr11link = "https://forms.office.com/r/fhxJ7tmmft";
+  const gr8link =
+    "https://forms.office.com/pages/responsepage.aspx?id=LV7swROUrEqeab99Ida2QlHxFL9pmx9Lh4NNEepFi3JUQlczNDVFUllYVzNNVlI2OFpLQVZCS1g3OC4u";
+  const gr9link =
+    "https://forms.office.com/pages/responsepage.aspx?id=LV7swROUrEqeab99Ida2QlHxFL9pmx9Lh4NNEepFi3JURVNLQldNQjQyVURZT05QOThCQTA5Q01EMC4u";
+  const gr10link =
+    "https://forms.office.com/pages/responsepage.aspx?id=LV7swROUrEqeab99Ida2QlHxFL9pmx9Lh4NNEepFi3JUMThWMVFQQVlQREtGT0I5Q1lUWUg1RUVFUy4u";
+  const gr11link =
+    "https://forms.office.com/pages/responsepage.aspx?id=LV7swROUrEqeab99Ida2QlHxFL9pmx9Lh4NNEepFi3JURVJSN1lLWDlNT0FGTVVJTkkzSFNONU9IMy4u";
 
-  const handleClick = (grade) => {
+  const getLink = (grade) => {
     /* switch case for grade */
     switch (grade) {
       case 8:
-        window.open(gr8link, "_blank");
-        console.log("Grade 8");
-        break;
+        return gr8link;
       case 9:
-        window.open(gr9link, "_blank");
-        console.log("Grade 9");
-        break;
+        return gr9link;
       case 10:
-        window.open(gr10link, "_blank");
-        console.log("Grade 10");
-        break;
+        return gr10link;
       case 11:
-        window.open(gr11link, "_blank");
-        console.log("Grade 11");
-        break;
+        return gr11link;
       default:
-        break;
+        return "";
     }
+  };
+
+  const handleClick = (grade) => {
+    setGrade(grade);
   };
 
   // before voting period
@@ -104,21 +105,45 @@ const SecureVote = () => {
   return (
     <div className={styles.page}>
       <h1 style={{ margin: "unset" }}>Voting Portal is Now Open!</h1>
-      <div className={styles.container}>
-        <h3>Please Select Grade to Cast Your Votes</h3>
-        <button className={styles.btnVote} onClick={() => handleClick(8)}>
-          Grade 8
-        </button>
-        <button className={styles.btnVote} onClick={() => handleClick(9)}>
-          Grade 9
-        </button>
-        <button className={styles.btnVote} onClick={() => handleClick(10)}>
-          Grade 10
-        </button>
-        <button className={styles.btnVote} onClick={() => handleClick(11)}>
-          Grade 11
-        </button>
-      </div>
+      {grade === 0 ? (
+        <div id="container" className={styles.container}>
+          <h3>Please Select Grade to Cast Your Votes</h3>
+          <button className={styles.btnVote} onClick={() => handleClick(8)}>
+            Grade 8
+          </button>
+          <button className={styles.btnVote} onClick={() => handleClick(9)}>
+            Grade 9
+          </button>
+          <button className={styles.btnVote} onClick={() => handleClick(10)}>
+            Grade 10
+          </button>
+          <button className={styles.btnVote} onClick={() => handleClick(11)}>
+            Grade 11
+          </button>
+        </div>
+      ) : (
+        <>
+          <iframe
+            title="form"
+            width="640px"
+            height="480px"
+            src={`${getLink(grade)}&embed=true`}
+            frameborder="0"
+            marginwidth="0"
+            marginheight="0"
+            style={{ border: "none", maxWidth: "100%", maxHeight: "100vh" }}
+            allowfullscreen
+            webkitallowfullscreen
+            mozallowfullscreen
+            msallowfullscreen
+          >
+            {" "}
+          </iframe>
+          <button className={styles.btnMain} onClick={() => setGrade(0)}>
+            Back
+          </button>
+        </>
+      )}
     </div>
   );
 };
