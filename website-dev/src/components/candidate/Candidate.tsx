ade2ccae7f6086dcdcdaac574d120cd540980b66
peterpetep <peter.xydong@gmail.com>
2025-03-20 15:45:53 -0700

    better submission preview

--- website-dev/src/components/candidate/Candidate.tsx
+++ website-dev/src/components/candidate/Candidate.tsx
@@ -8,6 +8,7 @@ export function Candidate({ candidate, forceURL }: {candidate?: CandidateData , 
   
     const handleMouseEnter = () => {
       if(!forceURL) setText("View Profile");
+      else setText("Click for Preview")
     };
     const handleMouseLeave = () => {
       setText("");
@@ -31,11 +32,12 @@ export function Candidate({ candidate, forceURL }: {candidate?: CandidateData , 
   
     return (
       <>
-        {forceURL == undefined ? <CandidateModal
+        <CandidateModal
           open={showModal}
           onClose={() => setShowModal(false)}
           candidate={candidate}
-        ></CandidateModal> : <></>}
+          forceURL={forceURL}
+        ></CandidateModal>
   
         <div
           className="flex flex-col intersect:animate-fade-right justify-start items-center w-32 min-h-32 md:min-h-48 rounded-lg shadow-lg transition-all m-4 bg-background cursor-pointer 
@@ -54,12 +56,12 @@ group-hover:bg-background/50 group-hover:bg-blend-color"
               backgroundPosition: "top", // Center the image
             }}
           >
-            <h4 className="font-bold">{text}</h4>
+            <h4 className="font-bold text-center">{text}</h4>
           </div>
           <div className="flex flex-col justify-center items-center text-foreground py-1 text-center font-semibold text-sm md:text-base min-h-10 md:min-h-16">
             {candidate && candidate.firstName && candidate.lastName ? (
               `${candidate.firstName} ${candidate.lastName}`
-            ) : forceURL ? "Preview" : (
+            ) : (
               <div
                 style={{
                   display: "flex",
